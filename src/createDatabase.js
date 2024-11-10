92dc2d74106fc57b00b41f96b490edffa753ce09
Motlatech <153740969+Motlatech@users.noreply.github.com>
2024-11-10 14:09:32 +0530

    Update createDatabase.js

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,15 +1,13 @@
-const dotenv = require ('dotenv');
-dotenv.config();
+const subDb_URI = process.env.subDatabase;
 const mongoose = require ("mongoose");
 
-const subDb_URI = process.env.subDatabase;
+
 // create Database
 
 
 const connectDb = async () => {
     try{
         await mongoose.connect(subDb_URI);
-        console.log('connected to db')
     }catch(error){
       console.log(error);
     }
